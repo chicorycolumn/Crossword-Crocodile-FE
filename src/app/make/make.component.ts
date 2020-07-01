5c07e0d0d7b66e2ba123fd523f40c6dd41755bc8
C <chrismatusemail@gmail.com>
2020-07-01 09:28:57 +0100

    modifying time of build

--- src/app/make/make.component.ts
+++ src/app/make/make.component.ts
@@ -6,7 +6,7 @@ import * as Util from '../shared/utils';
 
 let deactivateSocket = 0; // dev switch
 let onlyShowResultBox = 0; // dev switch
-let timeOfBuild = 909; // dev note
+let timeOfBuild = 928; // dev note
 
 @Component({
   selector: 'app-make',
